aae64a2b80e28cee0ca1fc6f59980149951edb1d
ndcuong.cf@gmail.com <ndcuong.cf@gmail.com>
2020-08-18 23:22:05 +0700

    x

--- ClientSPA/src/App.tsx
+++ ClientSPA/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from 'Components/Home/Home'
+import { Route } from 'react-router-dom'
+import Login from 'Components/Login/Login'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    private logo = `${process.env.PUBLIC_URL}/logo192.png`
+    render() {
+        return (
+            <React.Fragment>
+                <Route path="/" component={Home} />
+                <Route path="/login" component={Login} />
+            </React.Fragment>
+        )
+    }
 }
 
-export default App;
+export default App
