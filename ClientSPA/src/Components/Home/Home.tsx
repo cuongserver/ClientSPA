067d1277e39e25f62bd7a134f0751f17cce127da
ndcuong.cf@gmail.com <ndcuong.cf@gmail.com>
2020-08-19 22:54:46 +0700

    a

--- ClientSPA/src/Components/Home/Home.tsx
+++ ClientSPA/src/Components/Home/Home.tsx
@@ -2,27 +2,22 @@ import React from 'react'
 import 'App.css'
 
 class Home extends React.Component {
-    private logo = `${process.env.PUBLIC_URL}/logo192.png`
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={this.logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        )
-    }
+	private logo = `${process.env.PUBLIC_URL}/logo192.png`
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={this.logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.tsx</code> and save to reload.
+					</p>
+					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+						Learn React
+					</a>
+				</header>
+			</div>
+		)
+	}
 }
 
 export default Home
