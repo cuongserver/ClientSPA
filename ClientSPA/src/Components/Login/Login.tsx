4598d719c549fb804e84ab1d6a599fd8c6d4555e
ndcuong.cf@gmail.com <ndcuong.cf@gmail.com>
2020-08-19 23:36:46 +0700

    x

--- ClientSPA/src/Components/Login/Login.tsx
+++ ClientSPA/src/Components/Login/Login.tsx
@@ -1,25 +1,24 @@
 import * as React from 'react'
-
 export interface Props {
-  children?: React.ReactNode
+	children?: React.ReactNode
 }
 
 export interface State {}
 
 class Login extends React.PureComponent {
-  constructor(props: Props) {
-    super(props)
+	constructor(props: Props) {
+		super(props)
 
-    this.state = {}
-  }
+		this.state = {}
+	}
 
-  render() {
-    return (
-      <React.Fragment>
-        s<h6>This is sparta</h6>
-      </React.Fragment>
-    )
-  }
+	render() {
+		return (
+			<React.Fragment>
+				s<h6>This is sparta</h6>
+			</React.Fragment>
+		)
+	}
 }
 
 export default Login
