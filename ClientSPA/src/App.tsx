fc8e50070c0e90ea8b3b292e0d817e70f7c099ba
ndcuong.cf@gmail.com <ndcuong.cf@gmail.com>
2020-08-19 22:00:08 +0700

    a

--- ClientSPA/src/App.tsx
+++ ClientSPA/src/App.tsx
@@ -4,15 +4,15 @@ import { Route } from 'react-router-dom'
 import Login from 'Components/Login/Login'
 
 class App extends React.Component {
-    private logo = `${process.env.PUBLIC_URL}/logo192.png`
-    render() {
-        return (
-            <React.Fragment>
-                <Route path="/" component={Home} />
-                <Route path="/login" component={Login} />
-            </React.Fragment>
-        )
-    }
+	private logo = `${process.env.PUBLIC_URL}/logo192.png`
+	render() {
+		return (
+			<React.Fragment>
+				<Route path="/" exact component={Home} />
+				<Route path="/login" exact component={Login} />
+			</React.Fragment>
+		)
+	}
 }
 
 export default App
