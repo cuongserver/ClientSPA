506822388f75934c65fbb23c9b27ddc57b9ecf43
ndcuong.cf@gmail.com <ndcuong.cf@gmail.com>
2020-12-29 17:33:57 +0700

    c

--- ClientSPA/src/component/main/main.breadcrumbs.component.tsx
+++ ClientSPA/src/component/main/main.breadcrumbs.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 import { StaticContext, withRouter } from 'react-router'
-import { Breadcrumbs, Typography, Link } from '@material-ui/core'
+import { Breadcrumbs, Typography, Link, Card } from '@material-ui/core'
 import { NavigateNext } from '@material-ui/icons'
 import { withTranslation, WithTranslation } from 'react-i18next'
 import { breadcrumbNameMap } from 'configs/config.routes'
@@ -20,27 +20,29 @@ class MainBreadcrumbsOrigin extends React.PureComponent<
 		const pathnames = history.location.pathname.split('/')
 		if (pathnames[1] === '' && pathnames.length === 2) pathnames.pop()
 		return (
-			<Breadcrumbs separator={<NavigateNext fontSize="small" />}>
-				{pathnames.map((path, idx) => {
-					const last = idx === pathnames.length - 1
-					let to = `${pathnames.slice(0, idx + 1).join('/')}`
-					if (to === '' && idx === 0) to = '/'
-					return last ? (
-						<Typography color="textPrimary" key={to}>
-							{this.props.t(breadcrumbNameMap[to])}
-						</Typography>
-					) : (
-						<Link
-							color="inherit"
-							onClick={() => history.push(to)}
-							key={to}
-							className="hov-pointer"
-						>
-							{this.props.t(breadcrumbNameMap[to])}
-						</Link>
-					)
-				})}
-			</Breadcrumbs>
+			<Card raised={false} className="p-t-3 p-b-3 p-l-10 p-r-10">
+				<Breadcrumbs separator={<NavigateNext fontSize="small" />}>
+					{pathnames.map((path, idx) => {
+						const last = idx === pathnames.length - 1
+						let to = `${pathnames.slice(0, idx + 1).join('/')}`
+						if (to === '' && idx === 0) to = '/'
+						return last ? (
+							<Typography color="textPrimary" key={to}>
+								{this.props.t(breadcrumbNameMap[to])}
+							</Typography>
+						) : (
+							<Link
+								color="inherit"
+								onClick={() => history.push(to)}
+								key={to}
+								className="hov-pointer"
+							>
+								{this.props.t(breadcrumbNameMap[to])}
+							</Link>
+						)
+					})}
+				</Breadcrumbs>
+			</Card>
 		)
 	}
 }
