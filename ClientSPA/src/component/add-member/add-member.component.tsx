bffebf46e7b2fb1fd4e066ab2162ad22d00f0441
ndcuong.cf@gmail.com <ndcuong.cf@gmail.com>
2021-01-05 17:05:42 +0700

    c

--- ClientSPA/src/component/add-member/add-member.component.tsx
+++ ClientSPA/src/component/add-member/add-member.component.tsx
@@ -3,6 +3,13 @@ import { TextField, Button } from '@material-ui/core'
 import { WithTranslation, withTranslation } from 'react-i18next'
 import * as yup from 'yup'
 import _ from 'lodash'
+import {
+	Formik,
+	FormikProps,
+	FieldArray,
+	FormikErrors,
+	yupToFormErrors,
+} from 'formik'
 
 interface IProps extends WithTranslation {}
 
@@ -31,13 +38,18 @@ interface IFormData {
 }
 
 const schema = yup.object().shape({
-	email: yup
-		.string()
-		.required('addmember-error-email-required')
-		.min(1, 'addmember-error-email-required'),
+	email: yup.string().required('addmember-error-email-required').min(3, 'def'),
 	extra: yup.array().of(
 		yup.object().shape({
-			extraValue: yup.string().required('abc').min(1, 'abc'),
+			extraValue: yup
+				.string()
+				.required('abc')
+				.min(3, 'xyz')
+				.test('unique', 'not unique', (value, context) => {
+					if (_.isEmpty(value)) return true
+					const list = context.options.context as Extra[]
+					return list.filter((extra) => extra.extraValue === value).length <= 1
+				}),
 		})
 	),
 })
@@ -143,80 +155,270 @@ class AddMemberOrigin extends React.PureComponent<IProps, IState> {
 		}
 	}
 
+	get errorByPath2() {
+		return (obj: FormikErrors<IFormData>, path: string) => {
+			return _.get(obj, path)
+		}
+	}
+
+	validateData = () => {}
 	render() {
 		const { t } = this.props
-		const { formData, formValidationErrors } = this.state
+		// return (
+		// 	<React.Fragment>
+		// 		<TextField
+		// 			id="login-username"
+		// 			label={t('addmember-label-email')}
+		// 			variant="outlined"
+		// 			fullWidth={false}
+		// 			size="small"
+		// 			name="email"
+		// 			value={formData.email}
+		// 			onChange={this.formHandleChange}
+		// 			helperText={
+		// 				this.errorByPath('email') !== undefined
+		// 					? t(this.errorByPath('email')!.message!)
+		// 					: ' '
+		// 			}
+		// 			FormHelperTextProps={{
+		// 				className: 'p-b-5',
+		// 				error: true,
+		// 			}}
+		// 			error={this.errorByPath('email') !== undefined}
+		// 		/>
+		// 		<React.Fragment>
+		// 			{formData.extra.length > 0 &&
+		// 				formData.extra.map((val, idx) => {
+		// 					return (
+		// 						val.enabled && (
+		// 							<div key={idx}>
+		// 								<TextField
+		// 									label="extra"
+		// 									variant="outlined"
+		// 									fullWidth={false}
+		// 									size="small"
+		// 									name={`extra[${idx}].extraValue`}
+		// 									onChange={this.formHandleChange}
+		// 									value={val.extraValue}
+		// 									helperText={
+		// 										this.errorByPath(`extra[${idx}].extraValue`) !==
+		// 										undefined
+		// 											? t(
+		// 													this.errorByPath(`extra[${idx}].extraValue`)!
+		// 														.message!
+		// 											  )
+		// 											: ' '
+		// 									}
+		// 									FormHelperTextProps={{
+		// 										className: 'p-b-5',
+		// 										error: true,
+		// 									}}
+		// 									error={
+		// 										this.errorByPath(`extra[${idx}].extraValue`) !==
+		// 										undefined
+		// 									}
+		// 								/>
+		// 								<Button
+		// 									data-index={idx}
+		// 									onClick={this.removeExtra}
+		// 									variant="contained"
+		// 									color="primary"
+		// 								>
+		// 									Remove
+		// 								</Button>
+		// 								{idx}
+		// 							</div>
+		// 						)
+		// 					)
+		// 				})}
+		// 		</React.Fragment>
+		// 		<button onClick={this.addExtra}>Add more</button>
+		// 		{JSON.stringify(formValidationErrors)}
+		// 	</React.Fragment>
+		//)
 		return (
-			<React.Fragment>
-				<TextField
-					id="login-username"
-					label={t('addmember-label-email')}
-					variant="outlined"
-					fullWidth={false}
-					size="small"
-					name="email"
-					value={formData.email}
-					onChange={this.formHandleChange}
-					helperText={
-						this.errorByPath('email') !== undefined
-							? t(this.errorByPath('email')!.message!)
-							: ' '
-					}
-					FormHelperTextProps={{
-						className: 'p-b-5',
-						error: true,
-					}}
-					error={this.errorByPath('email') !== undefined}
-				/>
-				<React.Fragment>
-					{formData.extra.length > 0 &&
-						formData.extra.map((val, idx) => {
-							return (
-								val.enabled && (
-									<div key={idx}>
-										<TextField
-											label="extra"
-											variant="outlined"
-											fullWidth={false}
-											size="small"
-											name={`extra[${idx}].extraValue`}
-											onChange={this.formHandleChange}
-											value={val.extraValue}
-											helperText={
-												this.errorByPath(`extra[${idx}].extraValue`) !==
-												undefined
-													? t(
-															this.errorByPath(`extra[${idx}].extraValue`)!
-																.message!
-													  )
-													: ' '
-											}
-											FormHelperTextProps={{
-												className: 'p-b-5',
-												error: true,
-											}}
-											error={
-												this.errorByPath(`extra[${idx}].extraValue`) !==
-												undefined
-											}
-										/>
-										<Button
-											data-index={idx}
-											onClick={this.removeExtra}
-											variant="contained"
-											color="primary"
-										>
-											Remove
-										</Button>
-										{idx}
-									</div>
-								)
-							)
-						})}
-				</React.Fragment>
-				<button onClick={this.addExtra}>Add more</button>
-				{JSON.stringify(formValidationErrors)}
-			</React.Fragment>
+			<Formik
+				initialValues={initFormData}
+				enableReinitialize={true}
+				validateOnChange={false}
+				validateOnMount={false}
+				validationSchema={schema}
+				/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+				onSubmit={(values, helpers) => {}}
+			>
+				{(props: FormikProps<IFormData>) => {
+					const { errors, values, handleChange, setErrors } = props
+					return (
+						<React.Fragment>
+							<TextField
+								id="login-username"
+								label={t('addmember-label-email')}
+								variant="outlined"
+								fullWidth={false}
+								size="small"
+								name="email"
+								value={values.email}
+								onChange={(e) => {
+									const err = { ...errors }
+									_.set(err, e.currentTarget.name, undefined)
+									handleChange(e)
+									setErrors(err)
+								}}
+								helperText={!_.isEmpty(errors.email) ? t(errors.email!) : ' '}
+								FormHelperTextProps={{
+									className: 'p-b-5',
+									error: true,
+								}}
+								error={!_.isEmpty(errors.email)}
+							/>
+							{/*
+							<React.Fragment>
+								{values.extra.length > 0 &&
+									values.extra.map((val, idx) => {
+										return (
+											<div key={idx}>
+												<TextField
+													label="extra"
+													variant="outlined"
+													fullWidth={false}
+													size="small"
+													name={`extra.${idx}.extraValue`}
+													onChange={handleChange}
+													value={val.extraValue}
+													// helperText={
+													// 	errors.extra !== undefined &&
+													// 	errors.extra[idx] !== undefined
+													// 		? errors.extra[idx]
+													// 		: ' '
+													// }
+													FormHelperTextProps={{
+														className: 'p-b-5',
+														error: true,
+													}}
+												/>
+												<Button
+													data-index={idx}
+													onClick={() => {
+														values.extra.splice(idx, 1)
+													}}
+													variant="contained"
+													color="primary"
+												>
+													Remove
+												</Button>
+												{idx}
+											</div>
+										)
+									})}
+							</React.Fragment>
+							<button
+								onClick={() => {
+									values.extra.push({
+										extraValue: '',
+										enabled: true,
+									})
+								}}
+							>
+								Add more
+							</button>
+							{JSON.stringify(values)}
+							*/}
+							<FieldArray name="extra">
+								{(arrayHelpers) => {
+									const { push, remove } = arrayHelpers
+									return (
+										<React.Fragment>
+											{values.extra.length > 0 &&
+												values.extra.map((val, idx) => {
+													return (
+														<div key={idx}>
+															<TextField
+																label="extra"
+																variant="outlined"
+																fullWidth={false}
+																size="small"
+																name={`extra[${idx}].extraValue`}
+																onChange={(e) => {
+																	const err = { ...errors }
+																	_.set(err, e.currentTarget.name, undefined)
+																	handleChange(e)
+																	setErrors(err)
+																}}
+																value={val.extraValue}
+																helperText={
+																	!_.isEmpty(errors.extra) &&
+																	!_.isEmpty(errors.extra![idx])
+																		? _.get(errors, `extra[${idx}].extraValue`)
+																		: ' '
+																}
+																FormHelperTextProps={{
+																	className: 'p-b-5',
+																	error: true,
+																}}
+																error={
+																	!_.isEmpty(errors.extra) &&
+																	!_.isEmpty(errors.extra![idx])
+																}
+															/>
+															<Button
+																data-index={idx}
+																onClick={() => {
+																	remove(idx)
+																}}
+																variant="contained"
+																color="primary"
+															>
+																Remove
+															</Button>
+															{idx}
+														</div>
+													)
+												})}
+											<button
+												onClick={() => {
+													push({
+														extraValue: '',
+														enabled: true,
+													})
+												}}
+											>
+												Add more
+											</button>
+											<pre>{JSON.stringify(values)}</pre>
+											<pre>{JSON.stringify(errors)}</pre>
+											<button
+												onClick={() => {
+													// try {
+													// 	await validateYupSchema(
+													// 		values,
+													// 		schema,
+													// 		false,
+													// 		values.extra
+													// 	)
+													// 	return undefined
+													// } catch (e) {
+													// 	props.setErrors(e)
+													// }
+													schema
+														.validate(values, {
+															abortEarly: false,
+															context: values.extra,
+														})
+														.catch((err) => {
+															props.setErrors(yupToFormErrors(err))
+														})
+												}}
+											>
+												Validate
+											</button>
+										</React.Fragment>
+									)
+								}}
+							</FieldArray>
+						</React.Fragment>
+					)
+				}}
+			</Formik>
 		)
 	}
 }
