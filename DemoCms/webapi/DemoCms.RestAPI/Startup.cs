b5be132007f117e4687fca0f735fa9b55a61abaf
ndcuong.cf@gmail.com <ndcuong.cf@gmail.com>
2021-01-28 17:53:42 +0700

    c

--- DemoCms/webapi/DemoCms.RestAPI/Startup.cs
+++ DemoCms/webapi/DemoCms.RestAPI/Startup.cs
@@ -31,11 +31,22 @@ namespace DemoCms.RestAPI
 			services.AddRepository();
 			services.AddServices();
 			services.AddHelpers();
+
+			// Register the Swagger generator, defining 1 or more Swagger documents
+			services.AddSwaggerGen();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			// Enable middleware to serve generated Swagger as a JSON endpoint.
+			app.UseSwagger();
+
+			// specifying the Swagger JSON endpoint.
+			app.UseSwaggerUI(options =>
+			{
+				options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+			});
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
